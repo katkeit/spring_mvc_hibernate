7f2b056e282e25acc000b5ad82e7cf9e247d3cb9
katkeit <katelyn.eitel@hotmail.com>
2023-02-15 19:54:11 -0500

    Initial push and commit.

---
+++ src/main/java/katkeit/com/github/spring_mvc_hibernate/model/pages/HomePage.java
@@ -0,0 +1,42 @@
+package katkeit.com.github.spring_mvc_hibernate.model.pages;
+
+/*
+ * author: Katelyn Eitel
+ * project: spring_mvc_hibernate
+ * date: 2023-02-13
+ *
+ * license: Apache License 2.0
+ * description:
+ *		Copyright 2023 Katelyn Eitel 
+ * 		Licensed under the Apache License, Version 2.0 (the "License");
+ * 		you may not use this file except in compliance with the License.
+ * 		You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *	
+ *		Unless required by applicable law or agreed to in writing, software
+ *		distributed under the License is distributed on an "AS IS" BASIS,
+ *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *		See the License for the specific language governing permissions and
+ *		limitations under the License.
+ */
+
+public class HomePage
+{
+	private String message;
+	private String dateTime;
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getDateTime() {
+		return dateTime;
+	}
+	
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+}
